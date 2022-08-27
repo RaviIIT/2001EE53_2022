2ef76ae375cd688345a84699371cbba35ddaabd5
RaviIIT <ravi03122002@gmail.com>
2022-08-27 22:57:01 +0530

    message

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,8 @@
-def factorial(int x):
-    
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x*factorial(x-1)
 
 x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+print(factorial(x))
