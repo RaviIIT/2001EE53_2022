ee7db72e6e32d7b55fb1f666fa0407562cfe8785
Ravi Kumar <75074493+RaviIIT@users.noreply.github.com>
2022-12-01 01:42:23 +0530

    server_file with updated features

--- proj1/server_file.py
+++ proj1/server_file.py
@@ -1,10 +1,11 @@
 #imports
 import socket
-from tkinter import messagebox 
 import threading
 
-# hashtable for username and passwords
-username_hashtable = {}
+# map for username and passwords
+username_password = {}
+# map for username and email 
+username_mail = {}
 
 class ChatServer:
     # list of clients
@@ -40,25 +41,41 @@ class ChatServer:
                 break
             self.last_received_message = incoming_buffer.decode('utf-8')
             
-            # reset password encoded with this string pattern
-            if '--reset--' in str(self.last_received_message):
+            print(self.last_received_message)
+            
+            # new user sign up 
+            if '--pass--' in str(self.last_received_message):
+                print('NEW USER TRYING TO JOIN.')
+                new_usr_name = str(self.last_received_message).split('--pass--')[0]
+                new_usr_pass = str(self.last_received_message).split('--pass--')[1].split('--mail--')[0]
+                new_usr_mail = str(self.last_received_message).split('--mail--')[1]
+                
+                username_password[new_usr_name] = new_usr_pass             
+                username_mail[new_usr_name] = new_usr_mail
+                print('New username : ', new_usr_name, ' and New Password : ', new_usr_pass)   
+            elif '--reset--' in str(self.last_received_message):                                # reset password
                 new_usr_name = str(self.last_received_message).split('--reset--')[0]
                 new_usr_pass = str(self.last_received_message).split('--reset--')[1]
-                username_hashtable[new_usr_name] = new_usr_pass
-            elif '=' in str(self.last_received_message):                
-                usr_name = str(self.last_received_message).split('=')[0]
+                #changed password into map
+                username_password[new_usr_name] = new_usr_pass
+            elif '=' in str(self.last_received_message):                                        # new user joined
+                usr_name = str(self.last_received_message).split('=')[0].split('joined:')[1]
                 usr_pass = str(self.last_received_message).split('=')[1]
-                
                 self.last_received_message = str(self.last_received_message).split('=')[0]
                 # new user
-                if usr_name not in username_hashtable:
-                    username_hashtable[usr_name] = usr_pass
+                if usr_name not in username_password:
+                    # not registered user
+                    username_password[usr_name] = usr_pass
                     self.broadcast_to_all_clients(s)  # send to all clients    
+                    self.last_received_message = 'error'
+                    self.broadcast_to_all_clients(s)
                 else:
-                    if str(username_hashtable[usr_name]) != str(usr_pass):          #incorrect password
+                    # registered user with wrong credentials entered
+                    if str(username_password[usr_name]) != str(usr_pass):          #incorrect password
+                        print('incorrect password')
                         self.last_received_message = 'error'
                         self.broadcast_to_all_clients(s)
-                    else:
+                    else:                                                         # correct password entered
                         self.broadcast_to_all_clients(s)  # send to all clients   
             else:
                 self.broadcast_to_all_clients(s)  # send to all clients   
@@ -75,19 +92,18 @@ class ChatServer:
     def receive_messages_in_a_new_thread(self):
         while True:
             client = s, (ip, port) = self.server_socket.accept()
-            # added client
-            self.add_to_clients_list(client)
             print('Connected to ', ip, ':', str(port))
             # receive messages called
             t = threading.Thread(target=self.receive_messages, args=(s,))
-            t.start()
+            t.start()                                                       # starting thread
+            
+            # added client
+            self.add_to_clients_list(client)
             
     #add a new client 
     def add_to_clients_list(self, client):
         if client not in self.clients_list:
             self.clients_list.append(client)
-        print(len(self.clients_list))
-
 
 if __name__ == "__main__":
     ChatServer()
